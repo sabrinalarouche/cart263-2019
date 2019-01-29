004ab219d9232a3ba762ca54a950d7c7d04c4325
Sabrina <sabrina.larouche1998@gmail.com>
2019-01-29 11:55:13 -0500

    P1: Make fork move with .draggable

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -9,29 +9,12 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
+let fork;
+let mouth;
 
-// preload()
-//
-// Description of preload
-
-function preload() {
-
-}
-
-
-// setup()
-//
-// Description of setup
-
+$(document).ready(setup);
 function setup() {
-
-}
-
-
-// draw()
-//
-// Description of draw()
-
-function draw() {
-
+fork = $('#fork');
+mouth = $('#mouth');
+fork.draggable();
 }
