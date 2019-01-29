713524161ae879f7d670fd7b332a6577600404b8
Sabrina <sabrina.larouche1998@gmail.com>
2019-01-29 12:26:03 -0500

    P1: added meatball and positioned it using css

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -3,10 +3,15 @@
 /*****************
 
 Title of Project
-Author Name
+Sabrina Larouche
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
+
+Sources:
+Fork image: https://vector.me/browse/352861/fork
+Mouth image: https://pngtree.com/freepng/cartoon-open-ones-mouth-illustration_3509559.html
+Bowl image: https://www.kissclipart.com/pasta-png-clipart-spaghetti-with-meatballs-pasta-i-oxjscd/
 
 ******************/
 let fork;
