2b4c3cc8d3a00d7620fc926715cd938e444fd84e
Sabrina <sabrina.larouche1998@gmail.com>
2019-03-05 12:16:15 -0500

    P2: Added an area of bad images that appear when door is clicked on, either good or bad at random

--- projects/project2/js/script.js
+++ projects/project2/js/script.js
@@ -12,22 +12,47 @@ author, and this description to match your project!
 
 let leftDoor;
 let rightDoor;
+let order;
+let bad = [
+"bad1.png",
+"bad2.png",
+"bad3.png",
+"bad4.png"
+];
+
+let good= [
+"good1.png",
+"good2.png",
+"good3.png",
+"good4.png"
+];
 
 $(document).ready(setup);
 
 function setup() {
+
   leftDoor = $('#leftDoor');
   rightDoor = $('#rightDoor');
 
   leftDoor.on("click", function(){
-    leftDoor.attr('src','assets/images/ghost.jpeg')
-    //leftDoor.remove();
-
-  console.log("remove");
+  let r = Math.random();
+  if (r > .5)
+  {
+  leftDoor.attr('src','assets/images/bad/' + bad[Math.floor(Math.random() * bad.length)]);
+  }
+  else {
+  leftDoor.attr('src','assets/images/good/' + good[Math.floor(Math.random() * good.length)]);
+  }
   });
 
   rightDoor.on("click", function(){
-    rightDoor.remove();
-    console.log("remove");
+  let r = Math.random();
+    if (r > .5)
+    {
+    rightDoor.attr('src','assets/images/bad/' + bad[Math.floor(Math.random() * bad.length)]);
+    }
+    else {
+    rightDoor.attr('src','assets/images/good/' + good[Math.floor(Math.random() * good.length)]);
+    }
   });
 }
