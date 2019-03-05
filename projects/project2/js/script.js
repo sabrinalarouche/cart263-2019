e797f0512a70109b9b6fca048519ff9c24024146
Sabrina <sabrina.larouche1998@gmail.com>
2019-03-05 11:42:35 -0500

    P5: Displayed 2 doors that are droppable when clicked on to reveal an image

--- projects/project2/js/script.js
+++ projects/project2/js/script.js
@@ -9,3 +9,25 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
+
+let leftDoor;
+let rightDoor;
+
+$(document).ready(setup);
+
+function setup() {
+  leftDoor = $('#leftDoor');
+  rightDoor = $('#rightDoor');
+
+  leftDoor.on("click", function(){
+    leftDoor.attr('src','assets/images/ghost.jpeg')
+    //leftDoor.remove();
+
+  console.log("remove");
+  });
+
+  rightDoor.on("click", function(){
+    rightDoor.remove();
+    console.log("remove");
+  });
+}
