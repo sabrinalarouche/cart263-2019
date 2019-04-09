d99a70ec5dc7272a2e0e7f5a426ad7ea68217fb5
Sabrina <sabrina.larouche1998@gmail.com>
2019-04-09 09:56:34 -0400

    P3: Continuing what was done for A8

---
+++ projects/project3/js/Globe.js
@@ -0,0 +1,26 @@
+class Globe {
+  constructor(x,y) {
+    this.x = x;
+    this.y = y;
+    this.vy = -0.5;
+    this.originX = windowWidth/2;
+    this.originY = windowHeight/2;
+    this.bounce = 50;
+    this.image = loadImage('../assets/images/globe.png');
+  }
+
+  display() {
+      image(this.image,this.x,this.y);
+    }
+
+  move(){
+    this.y += this.vy;
+    console.log(this.y);
+  if(this.y < this.originY - this.bounce){
+    this.vy = -this.vy;
+  }
+  else if (this.y > this.originY){
+    this.vy = -this.vy;
+  }
+  }
+}
--- projects/project3/js/script.js
+++ projects/project3/js/script.js
@@ -14,8 +14,14 @@ author, and this description to match your project!
 //
 // Description of preload
 
+let bar;
+let globe;
+let boxes = [];
+let state = "LOADING"
+let cursor;
+let cursorX = 0;
+let cursorY = 0;
 function preload() {
-
 }
 
 
@@ -24,7 +30,18 @@ function preload() {
 // Description of setup
 
 function setup() {
-
+createCanvas(windowWidth,windowHeight);
+//globe = createImg('../assets/images/globe.png');
+globe = new Globe(windowWidth/2,windowHeight/2);
+bar = new Bar(windowWidth/2,windowHeight/2);
+boxes.push(new Box(0,50,350,350))
+boxes.push(new Box(450,50,350,350))
+boxes.push(new Box(850,50,350,350))
+boxes.push(new Box(0,450,350,350))
+boxes.push(new Box(450,450,350,350))
+boxes.push(new Box(850,450,350,350))
+imageMode(CENTER);
+cursor = loadImage('../assets/images/cursor.png');
 }
 
 
@@ -33,5 +50,38 @@ function setup() {
 // Description of draw()
 
 function draw() {
+noCursor();
+background(255);
+switch (state) {
+    case "LOADING":
+    {
+      displayLoading();
+      break;
+    }
 
+    case "START":
+    {
+      displayStart();
+      break;
+    }
+  }
+}
+
+function displayLoading(){
+globe.display();
+globe.move();
+bar.progress();
+}
+
+function displayStart(){
+for(let i = 0; i < boxes.length; i ++){
+  boxes[i].display();
+}
+fakeCursor();
+}
+
+function fakeCursor(){
+cursorX = lerp(cursorX,mouseX, 0.03);
+cursorY = lerp(cursorY,mouseY, 0.03);
+image(cursor,cursorX,cursorY);
 }
